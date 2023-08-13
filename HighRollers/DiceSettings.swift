7a4750e9c0bf88c8623d30b04ea74339f8241ef1
casalecchi <felipevilela191@gmail.com>
2023-08-12 22:24:04 -0300

    create new class DiceSettings

---
+++ HighRollers/DiceSettings.swift
@@ -0,0 +1,12 @@
+//
+//  DiceSettings.swift
+//  HighRollers
+//
+//  Created by Felipe Casalecchi on 12/08/23.
+//
+
+import Foundation
+
+class DiceSettings: ObservableObject {
+    @Published var diceSides = 6
+}
